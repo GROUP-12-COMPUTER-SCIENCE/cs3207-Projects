8de23b7210d5d018acb7bb89ac48ba6bc0968949
Clint G. L. Simiyu <69381872+Clint171@users.noreply.github.com>
2024-10-25 07:10:11 +0000

    Added parser for Zara

---
+++ zara-parser.py
@@ -0,0 +1,48 @@
+class ShiftReduceParser:
+def __init__(self, grammar):
+self.grammar = grammar  # List of grammar rules
+self.stack = []  # Stack to store tokens and non-terminals
+self.input_buffer = []  # Input tokens
+self.actions = []  # List of actions (shift/reduce)
+
+def parse(self, tokens):
+self.input_buffer = tokens[:]  # Copy of the input tokens
+self.stack = []
+self.actions = []
+
+while self.input_buffer:
+	self.shift()
+	
+	# Try to reduce after every shift
+	reduced = True
+	while reduced:
+		reduced = self.reduce()
+		
+		if not reduced and not self.input_buffer:
+			print("Syntax error: Unable to reduce further.")
+			break
+			
+			if len(self.stack) == 1 and self.stack[0] == 'BLOCK':
+				print("Parsing successful!")
+				else:
+					print("Parsing failed!")
+					
+					def shift(self):
+					# Move token from input buffer to stack
+					token = self.input_buffer.pop(0)
+					self.stack.append(token)
+					self.actions.append('SHIFT')
+					print(f"Shifted: {token}")
+					
+					def reduce(self):
+					# Try to match the top of the stack with a production rule
+					for rule in self.grammar:
+						if self.stack[-len(rule.rhs):] == rule.rhs:
+							# Perform reduction
+							self.stack = self.stack[:-len(rule.rhs)]  # Remove the RHS tokens
+							self.stack.append(rule.lhs)  # Push the LHS non-terminal
+							self.actions.append(f"REDUCE {rule.lhs} -> {rule.rhs}")
+							print(f"Reduced: {rule.lhs} -> {rule.rhs}")
+							return True
+							return False
+							
